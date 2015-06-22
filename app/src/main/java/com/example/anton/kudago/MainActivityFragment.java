730b3a74ee316e7f6ac5e68764d32c3010e6dc71
anton vishnevski <antonv@rhomobile.com>
2015-06-23 00:34:09 +0300

    remove expandable list to simple listview + change adapter class

--- app/src/main/java/com/example/anton/kudago/MainActivityFragment.java
+++ app/src/main/java/com/example/anton/kudago/MainActivityFragment.java
@@ -2,114 +2,64 @@ package com.example.anton.kudago;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.DragEvent;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
 
-class ExpandableListAdapter extends BaseExpandableListAdapter {
+class ExpandableListAdapter implements ListAdapter { // BaseExpandableListAdapter {
 
-    private Context _context;
-    private List<NewsLoader> mContentLoader;
+    private Context              mContext = null;
+    private List<NewsLoader>     mNews = null;
+    private List<CommentsLoader> mComments = null;
 
-    public ExpandableListAdapter(Context context, List<NewsLoader> loader) {
-        this._context = context;
-        this.mContentLoader = loader;
+    public ExpandableListAdapter(Context context, List<NewsLoader> news, List<CommentsLoader> comments) {
+        this.mContext = context;
+        this.mNews = news;
+        this.mComments = comments;
     }
-
-    @Override
-    public Object getChild(int groupPosition, int childPosititon) {
-        if (mContentLoader.get(groupPosition) != null && mContentLoader.get(groupPosition).getComments() != null) {
-            return this.mContentLoader.get(groupPosition).getComments().get(childPosititon);
-        }
-
-        return null;
-    }
-
-    @Override
-    public long getChildId(int groupPosition, int childPosition) {
-        return childPosition;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, final int childPosition,
-                             boolean isLastChild, View convertView, ViewGroup parent) {
-        return null; //convertView;
-    }
-
-    @Override
-    public int getChildrenCount(int groupPosition) {
-        if (mContentLoader.get(groupPosition) != null && mContentLoader.get(groupPosition).getComments() != null) {
-            return mContentLoader.get(groupPosition).getComments().size();
-        }
-
-        return 0;
-    }
-
-    @Override
-    public Object getGroup(int groupPosition) {
-        return this.mContentLoader.get(groupPosition);
-    }
-
-    @Override
-    public int getGroupCount() {
-        return this.mContentLoader.size();
-    }
-
-    @Override
-    public long getGroupId(int groupPosition) {
-        return groupPosition;
-    }
-
     boolean isf = true;
+
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+    public void registerDataSetObserver(DataSetObserver observer) {    }
 
-        NewsLoader headerData = (NewsLoader) getGroup(groupPosition);
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {    }
 
-        if (convertView == null) {
-                LayoutInflater infalInflater = (LayoutInflater) this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = infalInflater.inflate(
-                        isf == true ? R.layout.list_group0 : R.layout.list_group,
-                        null);
-                isf = false;
-        }
+    @Override
+    public int getCount() {
+        return mNews.size() + mComments.size();
+    }
 
-        TextView titleText = (TextView) convertView.findViewById(R.id.newsTitleText);
-        TextView contentText = (TextView) convertView.findViewById(R.id.newsContentText);
-        ImageView newsImage = (ImageView) convertView.findViewById(R.id.newsImageView);
+    @Override
+    public Object getItem(int position) {
+        if (position >= mNews.size())
+            return mComments.get(position - mNews.size());
 
-        titleText.setText(headerData.getNewsTitle());
-        titleText.setTypeface(null, Typeface.BOLD);
+        return mNews.get(position);
+    }
 
-//        contentText.settex
-        contentText.setText(headerData.getNewsContent());
-
-        if (newsImage != null) {
-            newsImage.setImageResource(R.drawable.news1);//.setImageDrawable(headerData.getNewsIcon());
-        }
-
-        return convertView;
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
@@ -118,8 +68,95 @@ class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return true;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        NewsLoader     newsData = null;
+        CommentsLoader commentData = null;
+
+        Object headerData = getItem(position);
+
+        if (headerData instanceof NewsLoader) {
+            newsData = (NewsLoader)headerData;
+        }
+        else if (headerData instanceof CommentsLoader) {
+            commentData = (CommentsLoader)headerData;
+        }
+        else {
+            return null;
+        }
+
+        LayoutInflater infalInflater = (LayoutInflater) this.mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (newsData != null)
+        {
+            convertView = infalInflater.inflate(
+                    isf == true ? R.layout.list_group0 : R.layout.list_group,
+                    null);
+            isf = false;
+
+
+            TextView titleText = (TextView) convertView.findViewById(R.id.newsTitleText);
+            TextView contentText = (TextView) convertView.findViewById(R.id.newsContentText);
+            ImageView newsImage = (ImageView) convertView.findViewById(R.id.newsImageView);
+
+            titleText.setText(newsData.getTitle());
+            titleText.setTypeface(null, Typeface.BOLD);
+
+            contentText.setText(newsData.getContent());
+
+            if (newsImage != null) {
+                newsImage.setImageResource(R.drawable.news1);//.setImageDrawable(newsData.getNewsIcon());
+            }
+        }
+        else if (commentData != null)
+        {
+            convertView = infalInflater.inflate(R.layout.list_comments, null);
+
+            TextView titleText = (TextView) convertView.findViewById(R.id.newsTitleText);
+            TextView contentText = (TextView) convertView.findViewById(R.id.newsContentText);
+            ImageView newsImage = (ImageView) convertView.findViewById(R.id.newsImageView);
+
+            titleText.setText(commentData.getAuthor());
+            titleText.setTypeface(null, Typeface.BOLD);
+
+            contentText.setText(commentData.getContent());
+
+            if (newsImage != null) {
+                newsImage.setImageResource(R.drawable.news2);//.setImageDrawable(newsData.getNewsIcon());
+            }
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0)
+            return 0;
+
+        if (position >= mNews.size())
+            return 1;
+
+        return 2;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
     }
 }
 
@@ -128,29 +165,56 @@ class ExpandableListAdapter extends BaseExpandableListAdapter {
  */
 public class MainActivityFragment extends Fragment {
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
+    ExpandableListAdapter mListAdapter;
+    ListView                mExpListView;
 
     private int currentIndex;
     private int messageCount = 4;
 
-    private List<NewsLoader> prepareListData() {
+    private List<CommentsLoader> prepareCommentListData() {
+        List<CommentsLoader> listData = new ArrayList<CommentsLoader>();
+
+        listData.add(new SimpleCommentsLoader("Elena",
+                "Comment text 1",
+                new Date(),
+                getResources().getDrawable(R.drawable.news1, null)));
+        listData.add(new SimpleCommentsLoader("Elena 1",
+                "Comment text 2",
+                new Date(),
+                getResources().getDrawable(R.drawable.news1, null)));
+        listData.add(new SimpleCommentsLoader("Elena 2",
+                "Comment text 3",
+                new Date(),
+                getResources().getDrawable(R.drawable.news1, null)));
+        listData.add(new SimpleCommentsLoader("Elena 3",
+                "Comment text 4",
+                new Date(),
+                getResources().getDrawable(R.drawable.news1, null)));
+        listData.add(new SimpleCommentsLoader("Elena 4",
+                "Comment text 5",
+                new Date(),
+                getResources().getDrawable(R.drawable.news1, null)));
+
+        return listData;
+    }
+
+    private List<NewsLoader> prepareNewsListData() {
          List<NewsLoader> listData = new ArrayList<NewsLoader>();
 
-        listData.add(new AsyncNewsLoader("What F1 Cars Would Look Like",
+        listData.add(new SimpleNewsLoader("What F1 Cars Would Look Like",
                 "There once was a time when the racing world was ruled by savage beasts. They were" +
                         " captured just before the snowy season, when noble brave men had one winter" +
                         " to tame this creature. After months of championship battle, a handful of the " +
                         "best animals were kept for another winter of training, while the others were set free again. ",
                 getResources().getDrawable(R.drawable.news1, null)));
 
-        listData.add(new AsyncNewsLoader("The Bloody Illegal World of Sand Mining",
+        listData.add(new SimpleNewsLoader("The Bloody Illegal World of Sand Mining",
                 "Photographer Adam Ferguson documents the environmental and human costs of illegal" +
                         " sand mining in India where rapid growth fuels a sometimes violent black market" +
                         " for one of the most basic raw building materials.",
                 getResources().getDrawable(R.drawable.news2, null)));
 
-        listData.add(new AsyncNewsLoader("Your Phone Aint as Safe as You Think",
+        listData.add(new SimpleNewsLoader("Your Phone Aint as Safe as You Think",
                 "Another week chock-full of hacks and vulns, and if you thought your password manager " +
                         "and cell phone were safe, youll want to pay close attention to the LastPass breach.",
                 getResources().getDrawable(R.drawable.news3, null)));
@@ -170,74 +234,35 @@ public class MainActivityFragment extends Fragment {
 
         final View fragamentView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        expListView = (ExpandableListView)fragamentView.findViewById(R.id.commentsListView);
+        mExpListView = (ListView)fragamentView.findViewById(R.id.commentsListView);
 
-        // preparing list data
-        prepareListData();
+        mListAdapter = new ExpandableListAdapter(fragamentView.getContext(),
+                prepareNewsListData(), prepareCommentListData());
 
-        listAdapter = new ExpandableListAdapter(fragamentView.getContext(), prepareListData());
+        final View headerView = inflater.inflate(R.layout.header, container, false);
+        final View footerView = inflater.inflate(R.layout.footer, container, false);
 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
+        ImageView imageView = (ImageView) headerView.findViewById(R.id.imageView2);
 
-        //////////////////////////////////////////////////////////////////////////////////////
-
-        final ImageSwitcher imageSwitcher = (ImageSwitcher) fragamentView.findViewById(R.id.rootImageSwitcher);
-
-        // Set the ViewFactory of the ImageSwitcher that will create ImageView object when asked
-        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-
-            public View makeView() {
-                // Create a new ImageView set it's properties
-                ImageView imageView = new ImageView(getActivity().getBaseContext());
-                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                imageView.setAdjustViewBounds(true);
-                return imageView;
-            }
-        });
-
-        // Declare the animations and initialize them
-        Animation in = AnimationUtils.loadAnimation(getActivity().getBaseContext(), android.R.anim.slide_in_left);
-        Animation out = AnimationUtils.loadAnimation(getActivity().getBaseContext(),android.R.anim.slide_out_right);
-
-        // set the animation type to imageSwitcher
-        imageSwitcher.setInAnimation(in);
-        imageSwitcher.setOutAnimation(out);
-        imageSwitcher.setImageResource(R.drawable.rootspb1);
-
-        imageSwitcher.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                currentIndex++;
-
-                // If index reaches maximum reset it
-                if (currentIndex == messageCount)
-                    currentIndex = 0;
-
-                imageSwitcher.setImageResource(imageIds[currentIndex]);
-            }
-        });
-
-        expListView.setOnTouchListener(new View.OnTouchListener() {
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setAdjustViewBounds(true);
+        imageView.setImageResource(R.drawable.rootspb1);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                int action = event.getAction();
-
-                switch (action) {
-                    case MotionEvent.ACTION_DOWN:
-                        imageSwitcher.setVisibility(View.GONE);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        imageSwitcher.setVisibility(View.VISIBLE);
-                        break;
+                if (MotionEvent.ACTION_OUTSIDE == event.getAction())
+                {
+                    return false;
                 }
-
-                v.onTouchEvent(event);
-                return true;
+                return false;
             }
         });
+
+        mExpListView.addHeaderView(headerView);
+        mExpListView.addFooterView(footerView);
+
+        // setting list adapter
+        mExpListView.setAdapter(mListAdapter);
 
         return fragamentView;
     }
